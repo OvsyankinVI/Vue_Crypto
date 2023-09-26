757a2a4c2fc585216548f372e1bec56852d513e1
OvsyankinVI <ovsyankin717@gmail.com>
2023-09-26 17:02:19 +0400

    add component "addGraph"

--- src/App.vue
+++ src/App.vue
@@ -93,49 +93,7 @@
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
 
-      <section class="relative"  v-if="selectedTicker">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.title }} - USD
-        </h3>
-        <div 
-          class="flex items-end border-gray-600 border-b border-l h-64"
-          ref="graph"
-        >
-          <div
-            v-for="(bar, idx) in normolizedGraph"
-            :key="idx"
-            :style="{ height: `${bar}%`}"
-            class="bg-purple-800 border w-10"
-          ></div>
-        </div>
-        <button
-          type="button"
-          class="absolute top-0 right-0"
-          @click="selectedTicker = null, grapg = []"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background:new 0 0 512 512"
-            xml:space="preserve"
-          >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+      <add-graph @max-graf-elements="maxGraph" @del-graph="delGraphNow" :graph="this.graph" :selectedTicker="this.selectedTicker"/>
     </div>
   </div>
 </template>
@@ -143,11 +101,13 @@
 <script>
 import { loadTicker } from './api'; 
 import AddTickers from './components/AddTickers.vue'
+import AddGraph from './components/AddGraph.vue'
 
 export default {
   name: 'App',
   components: {
-    AddTickers
+    AddTickers,
+    AddGraph
   },
   data() {
     return {
@@ -199,16 +159,7 @@ export default {
     hasNextPage() {
       return this.filteredTickers.length > this.endIndex
     },
-    normolizedGraph() {
-      const maxV = Math.max(...this.graph)
-      const minV = Math.min(...this.graph)
-       if (minV === maxV) {
-        return this.graph.map(() => 50)
-      }  
-      return this.graph.map(
-        price => 5 + ((price - minV) * 95) / (maxV - minV)
-      )
-    },
+
     pageStateOptions() {
       return {
         filter: this.filter,
@@ -217,13 +168,6 @@ export default {
     }
   },
   methods: {
-    calculateMaxGraphElements() {
-      if (!this.$refs.graph) {
-        return
-      }
-      this.maxGraphElements = this.$refs.graph.clientWidth / 38
-    },
-
     addPost(ticker) {
       const newPost = { title: ticker, price: "-" };
           this.tickers.push(newPost);
@@ -236,7 +180,7 @@ export default {
       setInterval(async() => {
         const exchangeData = await loadTicker(tickerName)
             if (this.tickers.find(c => c.title === tickerName) != undefined) {
-              this.tickers.find(c => c.title === tickerName).price = exchangeData.USD /* > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2) */
+              this.tickers.find(c => c.title === tickerName).price = exchangeData.USD
             }
             if (this.selectedTicker?.title === tickerName) {
               this.graph.push(exchangeData.USD)
@@ -244,20 +188,24 @@ export default {
             while (this.graph.length > this.maxGraphElements) {
               this.graph.shift()
             }
-          }, 5000)
+          }, 1000)
     },
     delPost(post) {
       this.tickers = this.tickers.filter(c => c != post)
+    },
+    maxGraph(maxGraphElem) {
+      this.maxGraphElements = maxGraphElem
+      console.log(maxGraphElem);
+    },
+    delGraphNow(delGraph) {
+      this.selectedTicker = delGraph
+      this.graph = []
     },
     select(t) {
       this.selectedTicker = t
     },
   },
   watch: {
-    selectedTicker() {
-      this.graph = [] 
-      this.$nextTick().then(this.calculateMaxGraphElements())
-    },
     paginatedTickers() {
       if (this.page > 1 && this.paginatedTickers.length === 0) {
         this.page -= 1
@@ -279,16 +227,10 @@ export default {
     }
   },
   mounted() {
-    window.addEventListener('resize', this.calculateMaxGraphElements)
-
     setTimeout(() =>
       {
         this.loading = false
       }, 500) 
   },
-
-  beforeUnmount() {
-    window.removeEventListener('resize', this.calculateMaxGraphElements)
-  }
 }
 </script>
